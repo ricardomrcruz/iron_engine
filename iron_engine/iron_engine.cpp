d111e568e21958cae282d6a07b8f07fbb7484422
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-09-02 01:41:27 +0200

    triangle

--- iron_engine/iron_engine.cpp
+++ iron_engine/iron_engine.cpp
@@ -3,6 +3,26 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+
+
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
+
+
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
@@ -15,7 +35,10 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello world", NULL, NULL);
+
+	
+
+	GLFWwindow* window = glfwCreateWindow(800, 480, "Hello world", NULL, NULL);
 	if (!window)
 	{
 
@@ -26,8 +49,6 @@ int main()
 	}
 
 	glfwMakeContextCurrent(window); 
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -35,15 +56,74 @@ int main()
 		return -1;
 	}
 
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+
+	GLuint shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f 
+	};
+
+
+	GLuint VAO, VBO;
+	
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+
+
+
+
+	
+
 
 	while (!glfwWindowShouldClose(window))
 	{
 
-		processInput(window);
+		//processInput(window);
 
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT); 
 
-		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 
@@ -51,9 +131,15 @@ int main()
 
 	}
 
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
 	glfwTerminate();
 	return 0;
 }
+
 
 void processInput(GLFWwindow* window)
 {
@@ -70,7 +156,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 	
-
 }
 
 
@@ -90,58 +175,3 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 
 
 
-
-
-
-
-
-//void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-//    glViewport(0, 0, width, height);
-//}
-//
-//int main() {
-//    std::cout << "Program started" << std::endl;
-//
-//    if (!glfwInit()) {
-//        std::cerr << "Failed to initialize GLFW" << std::endl;
-//        return -1;
-//    }
-//
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-//
-//    GLFWwindow* window = glfwCreateWindow(1024, 768, "Iron Engine", NULL, NULL);
-//    if (window == NULL) {
-//        std::cerr << "Failed to create GLFW window" << std::endl;
-//        glfwTerminate();
-//        return -1;
-//    }
-//
-//    glfwMakeContextCurrent(window);
-//    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-//
-//    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-//        std::cerr << "Failed to initialize GLAD" << std::endl;
-//        return -1;
-//    }
-//
-//    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
-//
-//    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-//
-//    while (!glfwWindowShouldClose(window)) {
-//        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-//        glClear(GL_COLOR_BUFFER_BIT);
-//
-//        
-//        glfwSwapBuffers(window);
-//        glfwPollEvents();
-//    }
-//
-//    glfwTerminate();
-//    std::cout << "Program ended" << std::endl;
-//    std::cout << "Press Enter to exit...";
-//    std::cin.get();
-//    return 0;
-//}
