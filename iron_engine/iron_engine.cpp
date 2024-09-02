fe48eb6498f38674fdb88eac4f6678b69da48f0a
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-09-03 01:47:00 +0200

    shader class

--- iron_engine/iron_engine.cpp
+++ iron_engine/iron_engine.cpp
@@ -58,23 +58,29 @@ int main()
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-
+	// Create Vertex Shader Object and get its reference
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	// Attach Vertex Shader source to the Vertex Shader Object
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	// Compile the Vertex Shader into machine code
 	glCompileShader(vertexShader);
 
+	// Create Fragment Shader Object and get its reference
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	// Attach Fragment Shader source to the Fragment Shader Object
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	// Compile the Vertex Shader into machine code
 	glCompileShader(fragmentShader);
 
-
+	// Create Shader Program Object and get its reference
 	GLuint shaderProgram = glCreateProgram();
-
+	// Attach the Vertex and Fragment Shaders to the Shader Program
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
-
+	// Wrap-up/Link all the shaders together into the Shader Program
 	glLinkProgram(shaderProgram);
 
+	// Delete the now useless Vertex and Fragment Shader objects
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
